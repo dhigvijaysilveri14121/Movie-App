11e8c309183fda9858e3d1d694ea7bf8abf1038c
Dhigvijay Silveri <dhigvijaysilveri14121@gmail.com>
2024-12-09 12:04:06 +0530

    made changes in fetching data from Api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import MovieListing from "./MovieListing";
+import MovieApi from "../common/MovieApi";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchMoviees = async () => {
+      try {
+        const response = await MovieApi.get(`?apikey={APIkey}`);
+        setMovies(response.data);
+        console.log(setMovies)
+      } catch (err) {
+        console.log("err", err);
+      }
+    };
+    fetchMoviees();
+  }, []);
   return (
-    <div>Home</div>
-  )
-}
+    <Box>
+      <MovieListing movies={movies}/>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import store from "./featuresRedux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider >
-      <App />
+    <Provider store={store}>
+      <App  />
     </Provider>
   </React.StrictMode>
 );
